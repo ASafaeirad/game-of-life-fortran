3f38f1d56c24b7cf88334e2d519cf40ce4053ff2
Alireza Safaierad <alireza.safaierad@gmail.com>
2026-04-07 01:14:33 +0200

    extract keyboard controll

--- src/main.f90
+++ src/main.f90
@@ -4,16 +4,15 @@ program main
    use argv_mod
    use grid_mod
    use game_of_life_mod
+   use terminal_mod
 
    implicit none
 
    integer(rk) :: rows, cols, file_grid(100, 100)
    integer(rk) :: generation = 1, generations = 10, timeout = 100000
-   integer(rk) :: remaining_sleep, sleep_chunk
+   integer(rk) :: keyboard_action
    integer(rk), parameter :: poll_interval = 10000
    character(len=256) :: input_path = 'input'
-   character(len=1) :: key
-   logical :: has_key
 
    call parse_cli_args(generations, input_path, timeout)
    call read_from_file(input_path, file_grid, rows, cols)
@@ -21,45 +20,21 @@ program main
    call enable_raw_input()
    call render()
 
-   do while(generation < generations)
-      call calculate_next_generation()
+   do
+      call poll_keyboard_during_timeout(timeout, poll_interval, keyboard_action)
+      if (keyboard_action == keyboard_action_reset) then
+         call reset_game()
+         generation = 0
+      else if (keyboard_action == keyboard_action_quit) then
+         call disable_raw_input()
+         print '(A)', 'Exiting...'
+         stop
+      end if
 
-      remaining_sleep = timeout
-
-      do while (remaining_sleep > 0)
-         sleep_chunk = min(poll_interval, remaining_sleep)
-         call usleep(sleep_chunk)
-         remaining_sleep = remaining_sleep - sleep_chunk
-
-         call read_key_nonblocking(key, has_key)
-         if (has_key) then
-            if (key == 'r' .or. key == 'R') then
-               call reset_game()
-               generation = 0
-               exit
-            end if
-            if (key == 'q' .or. key == 'Q') then
-               call disable_raw_input()
-               print '(A)', 'Exiting...'
-               stop
-            end if
-            if (key == 'p' .or. key == 'P') then
-               print '(A)', 'Paused. Press "p" to resume.'
-               do
-                  call read_key_nonblocking(key, has_key)
-                  if (has_key .and. (key == 'p' .or. key == 'P')) then
-                     print '(A)', 'Resuming...'
-                     exit
-                  end if
-                  call usleep(poll_interval)
-               end do
-            end if
-         end if
-      end do
-
-      generation = generation + 1
-      call render()
+      if (generation < generations) then
+         call calculate_next_generation()
+         generation = generation + 1
+         call render()
+      end if
    end do
-
-   call disable_raw_input()
 end program main
--- src/terminal_mod.f90
+++ src/terminal_mod.f90
@@ -1,7 +1,15 @@
 module terminal_mod
+   use ffi
+   use kinds_mod
+
    implicit none
    private
-   public :: clear_screen
+   public :: clear_screen, poll_keyboard_during_timeout
+   public :: keyboard_action_none, keyboard_action_reset, keyboard_action_quit
+
+   integer(rk), parameter :: keyboard_action_none = 0
+   integer(rk), parameter :: keyboard_action_reset = 1
+   integer(rk), parameter :: keyboard_action_quit = 2
 contains
 
    subroutine clear_screen()
@@ -10,4 +18,64 @@ contains
 
       print *, clear
    end subroutine clear_screen
+
+   subroutine poll_keyboard_during_timeout(timeout, poll_interval, action)
+      integer(rk), intent(in) :: timeout, poll_interval
+      integer(rk), intent(out) :: action
+
+      integer(rk) :: remaining_sleep, sleep_chunk
+
+      action = keyboard_action_none
+      remaining_sleep = timeout
+
+      do while (remaining_sleep > 0)
+         sleep_chunk = min(poll_interval, remaining_sleep)
+         call usleep(sleep_chunk)
+         remaining_sleep = remaining_sleep - sleep_chunk
+
+         call process_keyboard_input(poll_interval, action)
+         if (action /= keyboard_action_none) exit
+      end do
+   end subroutine poll_keyboard_during_timeout
+
+   subroutine process_keyboard_input(poll_interval, action)
+      integer(rk), intent(in) :: poll_interval
+      integer(rk), intent(out) :: action
+
+      character(len=1) :: key
+      logical :: has_key
+
+      action = keyboard_action_none
+      call read_key_nonblocking(key, has_key)
+
+      if (.not. has_key) return
+
+      select case (key)
+       case ('r')
+         action = keyboard_action_reset
+       case ('q')
+         action = keyboard_action_quit
+       case (' ')
+         call pause_until_resume(poll_interval)
+      end select
+   end subroutine process_keyboard_input
+
+   subroutine pause_until_resume(poll_interval)
+      integer(rk), intent(in) :: poll_interval
+
+      character(len=1) :: key
+      logical :: has_key
+
+      print '(A)', 'paused. press "space" to resume.'
+      do
+         call read_key_nonblocking(key, has_key)
+         if (has_key .and. key == ' ') then
+            print '(A)', 'resuming...'
+            exit
+         end if
+         call usleep(poll_interval)
+      end do
+   end subroutine pause_until_resume
+
+
 end module terminal_mod
